923e67b9d83da5ad9059b990a72ee750baa392e0
Radu Ungureanu <radu.ungureanu@outlook.com>
2018-05-14 10:23:13 +0100

    Initial Commit

---
+++ background.js
@@ -0,0 +1,22 @@
+browser.webRequest.onBeforeRequest.addListener(
+  function(request) {
+    if (request.url.includes("disable_polymer"))
+      return;
+
+    var url = request.url;
+    url += url.includes("?") ? "&" : "?";
+
+    return {
+      redirectUrl: url + "disable_polymer=1"
+    };
+  },
+  {
+    urls: [
+      "https://www.youtube.com/",
+      "https://www.youtube.com/watch*",
+      "https://www.youtube.com/results*",
+      "https://www.youtube.com/channel*"
+    ]
+  },
+  ["blocking"]
+);
